882279cdd079040d9797fcbfcd7622fc01950f14
Nikos Roussos <nikos@roussos.cc>
2021-06-25 10:01:11 +0300

    PLANET-6044 Expand container image to run all the acceptance tests
    Ref: https://jira.greenpeace.org/browse/PLANET-6044
    
    ---
    
    Changes done for this:
    - Use composer file to install all required dependencies
    - Use builder as the parent image in order to have helm (needed for
    interacting with a live instance)
    - Copy all Codeception configuration (mainly the _support folder) from
    base repo
    - Add test suite modules configuration (WPDb, WPWebDriver, WPCLI)
    - Expand the codeceptionify script to get access to the live instance

--- src/bin/codeceptionify.sh
+++ src/bin/codeceptionify.sh
@@ -21,7 +21,7 @@
 #   └── tests.suite.yml
 #
 
-set -eu
+set -eux
 
 destination="${1:-}"
 
@@ -30,8 +30,40 @@ if [[ -z "$destination" ]]; then
   exit 1
 fi
 
-rsync \
-	-av \
-	--ignore-existing \
-	/home/circleci/codeception/ \
-	"$destination"
+rsync -av --ignore-existing /home/circleci/codeception/ "$destination"
+
+git config --global user.email "${GIT_USER_EMAIL}"
+git config --global user.name "CircleCI Bot"
+
+git clone --depth 1 https://github.com/greenpeace/planet4-master-theme.git
+
+rsync -av planet4-master-theme/tests/acceptance/ tests/
+
+git clone --depth 1 https://github.com/greenpeace/planet4-plugin-gutenberg-blocks.git
+
+rsync -av planet4-plugin-gutenberg-blocks/tests/acceptance/ tests/
+
+WP_DB_USERNAME_DC=$(echo "${WP_DB_USERNAME}" | base64 -d)
+WP_DB_PASSWORD_DC=$(echo "${WP_DB_PASSWORD}" | base64 -d)
+WP_TEST_USER_DC=$(echo "${WP_TEST_USER_DC}" | base64 -d)
+echo "$SQLPROXY_KEY" | base64 -d >/home/circleci/key.json
+export GOOGLE_APPLICATION_CREDENTIALS="/home/circleci/key.json"
+export WP_DB_USERNAME_DC
+export WP_DB_PASSWORD_DC
+export WP_TEST_USER_DC
+
+activate-gcloud-account.sh
+
+make -C .. prepare-helm
+
+cloud_sql_proxy -instances="${GOOGLE_PROJECT_ID}:us-central1:${CLOUDSQL_INSTANCE}=tcp:3306" &
+
+sleep 2
+
+test_account_add.sh
+
+codecept run --xml=junit.xml --html
+
+test_account_remove.sh
+
+rm -f /home/circleci/key.json
---
+++ src/codeception/codeception/_support/Step/Acceptance/Admin/NavigationSteps.php
@@ -0,0 +1,73 @@
+<?php
+namespace Step\Acceptance\Admin;
+
+use Page\Acceptance\Admin\Navigation;
+
+/**
+ * Navigation: steps for navigating in admin interface
+ */
+class NavigationSteps
+{
+    /**
+     * @var \AcceptanceTester;
+     */
+    protected $tester;
+
+    /**
+     * @var GutenbergEditorPage
+     */
+    private $page;
+
+    public function __construct(
+        \AcceptanceTester $I
+    ) {
+        $this->tester = $I;
+    }
+
+    /**
+     * @example I navigate to "Posts > Add new"
+     *
+     * @When I navigate to :pagepath
+     */
+    public function iNavigateTo(string $pagepath): void
+    {
+        $I = $this->tester;
+        $I->amOnPage(Navigation::pageLink($pagepath));
+    }
+
+    /**
+     * @Given I open the dashboard
+     */
+    public function iOpenTheDashboard(): void
+    {
+        $I = $this->tester;
+        $I->amOnPage(Navigation::pageLink('Dashboard'));
+    }
+
+    /**
+     * @Given I am on a new post page
+     */
+    public function iAmOnANewPostPage(): void
+    {
+        $I = $this->tester;
+        $I->amOnPage(Navigation::pageLink('Posts > Add New'));
+    }
+
+    /**
+     * @Given I am on a import campaign page
+     */
+    public function iAmOnAImportCampaignPage(): void
+    {
+      $I = $this->tester;
+      $I->amOnPage(Navigation::pageLink('Tools > Import'));
+    }
+
+    /**
+     * @Given I am on a new campaign page
+     */
+    public function iAmOnANewCampaignPage()
+    {
+      $I = $this->tester;
+      $I->amOnPage(Navigation::pageLink('Campaigns > Add New'));
+    }
+}
